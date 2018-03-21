83aab9786ec2db449683143118900643d6895867
rolljee <n.riquelmebareiro@gmail.com>
2018-03-21 17:58:27 +0100

    Rework entire process

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
-const _ = require('lodash');
-const commandLineArgs = require('command-line-args')
-const ProgressBar = require('progress');
-
+const co = require('co');
 const {
 	translate,
 	detectLanguage,
 	wordAlternatives,
 	translateWithAlternatives
 } = require('deepl-translator');
+const commandLineArgs = require('command-line-args')
+const commandLineUsage = require('command-line-usage')
 
 const optionDefinitions = [
 	{ name: 'verbose', alias: 'v', type: Boolean },
@@ -16,74 +15,80 @@ const optionDefinitions = [
 	{ name: 'lang', alias: 'l', type: String },
 	{ name: 'input', alias: 'i', type: String },
 	{ name: 'output', alias: 'o', type: String },
+	{ name: 'shitf', alias: 's', type: String },
 ];
 
+const sections = [
+	{
+		header: 'i18n-File-translator',
+		content: 'Use deepl to translate a json i18 file to an other language'
+	},
+	{
+		header: 'Options',
+		optionList: [
+			{
+				name: 'help',
+				description: 'Print this usage guide.'
+			},
+			{
+				name: 'input',
+				typeLabel: 'String',
+				description: 'The input path of the process.'
+			},
+			{
+				name: 'output',
+				typeLabel: 'String',
+				description: 'The output path of the process.'
+			},
+			{
+				name: 'lang',
+				typeLabel: 'String',
+				description: 'The input to process.'
+			},
+			{
+				name: 'shitf',
+				typeLabel: 'String',
+				description: 'The time you want to add between each translation (used because API rate limite of deepL is low)'
+			},
+		]
+	}
+]
+
+const usage = commandLineUsage(sections)
 const options = commandLineArgs(optionDefinitions);
 
-let data;
-let moc;
-let lang;
-
-if (!options.input) {
-	throw new Error('input is not set');
-} else {
-	data = fs.readFileSync(options.input);
-	moc = JSON.parse(data);
+function verify() {
+	return !!options.lang && !!options.shift && !!options.input && !!options.output;
 }
 
-if (!options.lang) {
-	throw new Error('Lang is not set');
-} else {
-	lang = options.lang.toUpperCase();
+if (options.help) {
+	console.log(usage);
+	process.exit();
+} else if (!verify()) {
+	console.log(usage);
+	process.exit();
 }
 
-if (!options.output) {
-	throw new Error('ouput is not set');
-}
-
-let numberOfCall = 0;
-let numOfCallResolved = 0;
-let bar;
-
-translateObj = (params, outputLang, callback) => {
-	_.forIn(params, (value, key) => {
-		if (_.isObject(value)) {
-			translateObj(value, outputLang, callback);
-		} else {
-			numberOfCall++; // count for number of call
-			setTimeout(() => {
-				translate(value, outputLang)
-					.then(res => {
-						if (!bar) {
-							bar = new ProgressBar('translating [:bar] :percent :etas', {
-								complete: '=',
-								incomplete: ' ',
-								width: 50,
-								total: numberOfCall
-							});
-							bar.tick();
-						} else {
-							bar.tick();
-						}
-						numOfCallResolved++; // count for number of callback
-						params[key] = res.translation;
-						if (numberOfCall === numOfCallResolved) {
-							callback(null, moc); // once all callback arrived means that process ended
-						}
-					})
-					.catch((e) => {
-						callback(e, null);
-					})
-			}, numberOfCall * 2000) // avoid the api call limit
-		}
-	});
-};
-
-translateObj(moc, lang, (err, result) => {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log(`Translation file available at ${options.output}`);
-		fs.writeFileSync(options.output, JSON.stringify(result));
+try {
+	const JSONInputData = JSON.parse(fs.readFileSync(options.input));
+	const lang = options.lang.toUpperCase();
+	function iterate(actualObject) {
+		Object.entries(actualObject).forEach(([key, value]) => {
+			if (typeof value === 'object') {
+				iterate(value);
+			} else {
+				setTimeout(function () {
+					co(function* () {
+						const res = yield translate(value, lang);
+						actualObject[key] = res.translation;
+						fs.writeFileSync(options.output, JSON.stringify(JSONInputData));
+					});
+				}, Number(options.shift));
+			}
+		});
 	}
-});
+	iterate(JSONInputData);
+} catch (e) {
+	console.log(usage);
+	process.exit();
+}
